be75e5027f81b68b615109abd3dc9fda7d4c0ac4
Sales Filho <salesfilho@gmail.com>
2016-06-01 08:17:12 -0300

    Retirada do banner do springboot no startup da aplicação.

--- src/main/java/br/prof/salesfilho/sna/SnaApplication.java
+++ src/main/java/br/prof/salesfilho/sna/SnaApplication.java
@@ -1,11 +1,14 @@
 package br.prof.salesfilho.sna;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SnaApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SnaApplication.class, args);
+        SpringApplication app = new SpringApplication(SnaApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
     }
 }
